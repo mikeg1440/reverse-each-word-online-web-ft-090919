e4064ea270ec16c2a12125f6b3c1c64409af4643
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 14:23:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,22 @@
 
+# def reverse_each_word(sentance)
+  
+#   reversed_sentance = []
+  
+#   sentance.split(" ").each{|word| reversed_sentance << word.reverse }
+  
+#   reversed_sentance.join(" ")
+# end
+
+
+
 def reverse_each_word(sentance)
   
-  reversed_sentance = []
+  sentance.split.collect |word| do
+    
+    word.reverse
+    
+  end
   
-  sentance.split(" ").each{|word| reversed_sentance << word.reverse }
   
-  
-  
-  reversed_sentance.join(" ")
 end
-
-
-# puts reverse_each_word("Hello there, and how are you?")
