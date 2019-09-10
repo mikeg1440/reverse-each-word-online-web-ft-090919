952b52a7d57b01361ad55ce801957f38fe996e0e
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-10 14:17:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentance)
   
   reversed_sentance = ""
   
-  sentance.each{|word| reversed_sentance << word.reverse }
+  sentance.split.each{|word| reversed_sentance << word.reverse }
   
   reversed_sentance
 end
